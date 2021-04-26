a6a0ef0263062b71c85504b592625f7c0c9d13f3
Brittany Loy <britloy91@gmail.com>
2021-04-26 19:51:17 -0400

    Update app.py

--- Feedback-App/app.py
+++ Feedback-App/app.py
@@ -5,34 +5,39 @@ app = Flask(__name__)
 
 ENV = 'dev'
 
-# Things to find out
-    # Did I set up the url database incorrectly? Read documentations 
-    # Create feedback table. In terminal try (from app import)?
 
+# Things to find out
+# Did I set up the url database incorrectly? Read documentations
+# Create feedback table. In terminal try (from app import)?
+# Using mySQL 
+
+
+db_path = "mysql://root:root@localhost:3306/feedback_app_db"
 if ENV == 'dev':
     app.debug = True
-    app.config["SQLALCHEMY_DATABASE_URL"] = 'postgresql://postgres:Iwantbetter21@localhost/comments'
-else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_path 
     app.debug = False
-    app.config["SQLALCHEMY_DATABASE_URL"] = ''
+    app.config['SQLALCHEMY_DATABASE_URL'] = ''
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] - False
+db = SQLAlchemy(app)
 
-    db = SQLAlchemy(app)
+# this is code is not working
 
-    class Feedback(db.Model):
-        __tablename__ = 'feedback'
-        id = db.Colum(db.Integer, primary_key=True)
-        customer = db.Column(db.String(200), unique=True)
-        rating = db.Colum(db,Integer)
-        business = db.Colum(db.String(200))
-        comments = db.Colum(db.Text())
+  class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    rating = db.Column(db, Integer)
+    business = db.Column(db.String(200))
+    comments = db.Column(db.Text())
 
-        def __init__(self, customer, rating, business, comments):
-                self.customer = customer
-                self.rating = rating
-                self.business = business
-                self.comments = comments
+   def __init__(self, customer, rating, business, comments):
+    self.customer = customer
+    self.rating = rating
+    self.business = business
+    self.comments = comments
+
 
 @app.route('/')
 def index():
